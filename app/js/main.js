a0be3c5765ee1748fc98ea685079f5967a79c8f0
lolipop9900 <ngs282004@mail.ru>
2022-08-18 21:28:58 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
-$(function(){
+$(function () {
+  $(".rate-star").starRating({
+    totalStars: 5,
+    initialRating: 5,
+    useGradient: false,
+    activeColor: "orange",
+    starSize: 12,
+    readOnly: true,
+  });
 
+  $(".product-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 
+  var mixer = mixitup(".products__inner-box");
 });
